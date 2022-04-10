a54303ea9934b507711c622ff48b7021346d5bc7
Alan Calvillo <alan.calvillo@wizeline.com>
2022-04-10 18:48:09 -0500

    swagger to implement proper docs

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require('swagger-ui-express');
+const swagger = require('../swagger');
+const specs = swaggerJsdoc(swagger.swaggerDocument);
+
 router.use('/users', require('./users.js'))
+router.use('/api-docs',swaggerUi.serve,swaggerUi.setup(specs));
 
 module.exports = router;
